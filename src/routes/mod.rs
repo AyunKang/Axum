8a7177e583e82c5c91debecbd50e1499950ea24b
Ayun Kang <lucy0969@naver.com>
2024-02-01 23:28:14 +0900

    added handler for path variables

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,18 +1,21 @@
 mod hello_world;
 mod mirror_body_json;
 mod mirror_body_string;
+mod path_variable;
 
 use axum::{
-    routing::{patch, post},
+    routing::{get, patch, post},
     Router,
 };
 use hello_world::hello_world;
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
+use path_variable::path_variable;
 
 pub fn create_router() -> Router {
     Router::new()
         .route("/", patch(hello_world))
         .route("/mirror_body_string", post(mirror_body_string))
         .route("/mirror_body_json", post(mirror_body_json))
+        .route("/path_variable/:id", get(path_variable))
 }
