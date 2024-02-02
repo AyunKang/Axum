feba444157897c2ddfa13ddb8f808ca4de3f8230
Ayun Kang <lucy0969@naver.com>
2024-02-02 19:05:16 +0900

    added handler for mirroring custom header value

---
+++ src/routes/mirror_custom_header.rs
@@ -0,0 +1,7 @@
+use axum::http::HeaderMap;
+
+pub async fn mirror_custom_header(header: HeaderMap) -> String {
+    let message_value = header.get("x-message").unwrap();
+    let message = message_value.to_str().unwrap().to_owned();
+    message
+}
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,7 @@
 mod hello_world;
 mod mirror_body_json;
 mod mirror_body_string;
+mod mirror_custom_header;
 mod mirror_user_agent;
 mod path_variable;
 mod query_params;
@@ -12,6 +13,7 @@ use axum::{
 use hello_world::hello_world;
 use mirror_body_json::mirror_body_json;
 use mirror_body_string::mirror_body_string;
+use mirror_custom_header::mirror_custom_header;
 use mirror_user_agent::mirror_user_agent;
 use path_variable::path_variable;
 use query_params::query_params;
@@ -24,4 +26,5 @@ pub fn create_router() -> Router {
         .route("/path_variable/:id", get(path_variable))
         .route("/query_params", get(query_params))
         .route("/mirror_user_agent", get(mirror_user_agent))
+        .route("/mirror_custom_header", get(mirror_custom_header))
 }
