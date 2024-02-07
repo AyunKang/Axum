4599986b34c8e5f8a77c6b343487ad8d124b0086
Ayun Kang <lucy0969@naver.com>
2024-02-07 14:45:44 +0900

    connected sea-orm to the database

--- src/data/src/main.rs
+++ src/data/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use data::run;
+use dotenvy::dotenv;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let database_uri = dotenvy::var("DATABASE_URL").unwrap();
+    run(&database_uri).await;
 }
--- src/routes/validate_with_serde.rs
+++ src/routes/validate_with_serde.rs
@@ -3,8 +3,8 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 pub struct RequestUser {
-    username: Option<String>,
-    password: i32,
+    pub username: Option<String>,
+    pub password: i32,
 }
 
 pub async fn validate_with_serde(Json(user): Json<RequestUser>) {
